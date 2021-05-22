08dca6c225c9aad72bb493f7b3a352e9510e48b0
tushal012 <tushalkashyap11@gmail.com>
2021-05-22 15:58:05 +0530

    Welome to Address Book

---
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address book Program");
+        //new AddressBook();
+    }
+}
